b7d7362433ddb41fbda6fd8818502090574b573e
Ben Morrall <bemo56@hotmail.com>
2022-01-17 14:33:33 +1100

    Add login page

--- template.rb
+++ template.rb
@@ -89,8 +89,8 @@ def add_devise_routes
   route <<~DEVISE
     devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
     devise_scope :user do
-      get 'sign_in', :to => 'devise/sessions#new', :as => :new_user_session
-      delete 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
+      get 'sign_in', :to => 'users/sessions#new', :as => :new_user_session
+      delete 'sign_out', :to => 'users/sessions#destroy', :as => :destroy_user_session
     end
   DEVISE
 end
